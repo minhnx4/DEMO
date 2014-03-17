3f0d232d66e24fe4102d78a1d11bab42dca4a45c
anhlt <tuananh.kirimaru@gmail.com>
2014-03-17 08:57:42 +0000

    Pull error

--- Config/database.php
+++ Config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'tuananh',
+		'password' => '',
  		'database' => 'elearning'
 		);
 }
